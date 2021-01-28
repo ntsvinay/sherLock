162b37da06cde95b30cea0c04329075b1ee60a2e
unify <vinay@unify.com>
2021-01-28 14:41:09 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';    
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {RetailComponent} from './retail/retail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' },    
+{path:'home-page',component:HomePageComponent},
+{path:'retail',component:RetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent=[HomePageComponent,RetailComponent]
+
